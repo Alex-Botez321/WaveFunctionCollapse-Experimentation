6446240dd78287cdcf8c07e2407b5c6494642917
Alex Botez <stuffed.panda1423@gmail.com>
2026-01-24 14:01:41 +0000

    Registering classes for adjacency rules

--- Source/WaveFunctionCollapse/Private/DataCollector.cpp
+++ Source/WaveFunctionCollapse/Private/DataCollector.cpp
@@ -5,6 +5,7 @@
 #include "RoomBase.h"
 #include "Engine.h"
 #include "EngineUtils.h"
+#include "DrawDebugHelpers.h"
 
 // Sets default values
 ADataCollector::ADataCollector()
@@ -18,11 +19,45 @@ ADataCollector::ADataCollector()
 void ADataCollector::BeginPlay()
 {
 	Super::BeginPlay();
-	int i = 0;
 	UWorld* World = GetWorld();
 
 	for (TActorIterator<ARoomBase> ItActor(World); ItActor; ++ItActor)
 	{
 		UE_LOG(LogTemp, Log, TEXT("[%hs]: found simple actor: %s"), __FUNCTION__, *ItActor->GetName());
+
+		FVector Start = ItActor->GetActorLocation();
+		float Distance = 300.0f;
+
+		TArray<FVector> Directions;
+		Directions.SetNum(4);
+
+		Directions[0] = ItActor->GetActorForwardVector();
+		Directions[1] = -ItActor->GetActorForwardVector();
+		Directions[2] = ItActor->GetActorRightVector();
+		Directions[3] = -ItActor->GetActorRightVector();
+
+		for (int i = 0; i < 4; i++)
+		{
+			Directions[i].Normalize();
+
+			FVector End = Start + (Directions[i] * Distance);
+
+			FHitResult Hit;
+
+			FCollisionQueryParams Params;
+			Params.AddIgnoredActor(*ItActor);
+
+			bool bHit = GetWorld()->LineTraceSingleByChannel(Hit, Start, End, ECC_Visibility, Params);
+
+			if (bHit)
+			{
+				UE_LOG(LogTemp, Log, TEXT("Hit %s at ^s"), *Hit.GetActor()->GetName(), *Hit.ImpactPoint.ToString());
+				DrawDebugLine(GetWorld(), Start, End, FColor::Green, true, 100.0f, 0, 1.5f);
+			}
+			else
+				DrawDebugLine(GetWorld(), Start, End, FColor::Red, true, 100.0f, 0, 1.5f);
+			
+		}
+		
 	}
 }
